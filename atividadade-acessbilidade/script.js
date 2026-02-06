afed1b2b615c79e4244daef63ae01035a19727b6
Pedro de Oliveira <pedrokssp@gmail.com>
2026-02-06 16:27:31 -0300

    Add files via upload

---
+++ atividadade-acessbilidade/script.js
@@ -0,0 +1,9 @@
+function modoEscuro () {
+    document.body.style.backgroundColor = "#000";
+    document.body.style.color = "#fff";
+}
+
+function modoClaro () {
+    document.body.style.backgroundColor = "#fff";
+    document.body.style.color = "#000";
+}
